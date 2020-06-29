97b988ebcb29e4ce54edf5e50bb5fdfac3e5e038
Sadra Hakim <sadrahkm0000@gmail.com>
2020-06-29 23:02:46 +0430

    organize folders

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-    <script type="application/javascript" src="jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Enigma Machine</title>
+    <script type="application/javascript" src="js/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
             integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
             crossorigin="anonymous"></script>
@@ -18,7 +18,7 @@
             crossorigin="anonymous"></script>
     <!--    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
                 integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>-->
-    <script src="main.js" type="application/javascript"></script>
+    <script src="js/main.js" type="application/javascript"></script>
 
 </head>
 <body>
